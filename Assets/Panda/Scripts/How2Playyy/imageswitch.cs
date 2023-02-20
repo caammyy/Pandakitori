238539c5595b7e018c8213c7fd53d9313b5a5f80
aawyq <ashley.w@digipen.edu>
2023-02-20 23:38:21 +0800

    Ashley Commit 2
    added how to play after start button

---
+++ Assets/Panda/Scripts/How2Playyy/imageswitch.cs
@@ -0,0 +1,77 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class imageswitch : MonoBehaviour
+{
+
+    public GameObject[] background;
+    int index;
+    public Button next;
+    public Button play;
+    public Button previous;
+
+    void Start()
+    {
+        index = 0;
+        play.gameObject.SetActive(false);
+        previous.gameObject.SetActive(false);
+    }
+
+
+    void Update()
+    {
+        if (index >= 5)
+            index = 5;
+
+        if (index < 0)
+            index = 0;
+
+
+
+        if (index == 0)
+        {
+            background[0].gameObject.SetActive(true);
+            previous.gameObject.SetActive(false);
+        }
+
+        if (index == 1)
+        {
+            previous.gameObject.SetActive(true);
+        }
+
+        if (index == 5)
+        {
+            next.gameObject.SetActive(false);
+            play.gameObject.SetActive(true);
+        }
+    }
+
+    public void Next()
+    {
+        index += 1;
+
+        for (int i = 0; i < background.Length; i++)
+        {
+            background[i].gameObject.SetActive(false);
+            background[index].gameObject.SetActive(true);
+        }
+        Debug.Log(index);
+    }
+
+    public void Previous()
+    {
+        index -= 1;
+
+        for (int i = 0; i < background.Length; i++)
+        {
+            background[i].gameObject.SetActive(false);
+            background[index].gameObject.SetActive(true);
+        }
+        Debug.Log(index);
+    }
+
+
+}
