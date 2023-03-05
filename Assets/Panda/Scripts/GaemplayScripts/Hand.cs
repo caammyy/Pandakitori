9faecb0a0b6673a19bc6c92f988b62053c854bc3
JoelYeet <joelyeeyongyi99@gmail.com>
2023-03-06 02:23:07 +0800

    test throwing anim

--- Assets/Panda/Scripts/GaemplayScripts/Hand.cs
+++ Assets/Panda/Scripts/GaemplayScripts/Hand.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Hand : MonoBehaviour
 {
+    public float Multiplier;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +14,12 @@ public class Hand : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.rotation = Quaternion.Euler(0.0f, 0.0f, Trajectory.Angle); 
+        // transform.rotation = Quaternion.Euler(0.0f, 0.0f, Trajectory.Angle); 
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        difference.Normalize();
+        float rotationz = (Mathf.Atan2(difference.y * -1,difference.x * -1) * Mathf.Rad2Deg) * Multiplier;
+
+        transform.rotation = Quaternion.Euler(0f,0f,rotationz);
         
     }
 }
