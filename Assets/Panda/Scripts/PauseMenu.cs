862d1516f7c84e7d96c6a420d5e0ecba3c3012e4
caammyy <cammylimzy@gmail.com>
2023-03-07 02:13:07 +0800

    enabled Allow Fullscreen Switch, pause menus for Level 1 and 2

--- Assets/Panda/Scripts/PauseMenu.cs
+++ Assets/Panda/Scripts/PauseMenu.cs
@@ -6,24 +6,44 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] public GameObject PauseMenuPanel;
+    public static bool GameIsPaused = false;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
 
     public void Pause()
     {
         PauseMenuPanel.SetActive(true);
         Time.timeScale = 0f;
+        GameIsPaused = true;
     }
     public void Resume()
     {
         PauseMenuPanel.SetActive(false);
         Time.timeScale = 1f;
+        GameIsPaused = false;
     }
     public void Restart()
     {
         Time.timeScale = 1f;
+        Debug.LogWarning(PlayerPrefs.GetInt("currentLevel"));
         SceneManager.LoadScene(PlayerPrefs.GetInt("currentLevel"));
     }
     public void Home()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(0);
     }
 
