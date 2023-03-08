d3048c4de566c8d3a88d926b83c5e1a4d0cc9e63
caammyy <cammylimzy@gmail.com>
2023-03-09 03:57:01 +0800

    how to play polished.

--- Assets/Panda/Scripts/How2Playyy/nextEvent.cs
+++ Assets/Panda/Scripts/How2Playyy/nextEvent.cs
@@ -9,86 +9,73 @@ public class nextEvent : MonoBehaviour
     public GameObject food;
     public GameObject customers;
 
-    public Button previous;
-    public Button next;
     public Button play;
 
     public Text tutorialText;
+    public Text inventoryText;
 
     int index;
-
-    //index 0: welcome to the tutorial
-    //index 1: WASD
-    //index 2: Press E to pick up
-    //index 3: Press F to clear
-    //index 4: Hold and let go to shoot the stick
-    //index 5: you're ready to play! feel free to continue playing.
+ 
+    //index 0: welcome to the tutorial, WASD
+    //index 1: Press E to pick up
+    //index 2: Press F to clear
+    //index 3: Hold and let go to shoot the stick
+    //index 4: you're ready to play! feel free to continue playing.
 
     // Start is called before the first frame update
     void Start()
     {
         index = 0;
-        play.gameObject.SetActive(false);
-        previous.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (index >= 5)
-            index = 5;
-
-        if (index < 0)
-            index = 0;
-
-
-
         if (index == 0)
         {
-            customers.gameObject.SetActive(false);
-            food.gameObject.SetActive(false);
-            previous.gameObject.SetActive(false);
-            tutorialText.text = "Welcome to the tutorial! Click Next to continue.";
+            tutorialText.text = "Welcome to the tutorial! \n Use W, A, S, and D to move around your kitchen!";
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
+            {
+                index = 1;
+            }
         }
 
         if (index == 1)
         {
-            customers.gameObject.SetActive(false);
-            previous.gameObject.SetActive(true);
-            food.gameObject.SetActive(false);
-            tutorialText.text = "You may use W, A, S, and D to move around your kitchen.";
-        }
-        else if (index == 2)
-        {
-            customers.gameObject.SetActive(false);
             food.gameObject.SetActive(true);
-            tutorialText.text = "Try picking up a piece of food to add to your Yakitori Stick! Press E to Pick Up.";
+            tutorialText.text = "Try adding a food piece to your Yakitori Stick! \n Move to the food piece and Press E to Pick Up!";
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (Inventory.AmountOfFood > 0)
+                {
+                    index = 2;
+                }
+            }
         }
-        else if (index == 3)
+        if (index == 2)
         {
-            customers.gameObject.SetActive(false);
-            tutorialText.text = "If you wish to remove the food from your stick, Press F to Clear.";
+            inventoryText.gameObject.SetActive(true);
+            tutorialText.text = "If you wish to remove the food from your stick, Press F to Clear!";
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                index = 3;
+            }
         }
-        else if (index == 4)
+        if (index == 3)
         {
+            inventoryText.gameObject.SetActive(false);
             customers.gameObject.SetActive(true);
-            tutorialText.text = "Let's try serving our 1st customer! Pick up a full stick of food! Then, hold down your LEFT mouse button and drag backwards and aim for the customer. Let go of your mouse button to shoot!";
+            tutorialText.text = "Let's serve our 1st customer! \n Follow the customer's order (above head) and PICK UP 2 food pieces! \n HOLD down your LEFT mouse button, DRAG backwards and AIM. \n Let go of the mouse button to shoot and serve!";
+            if (Inventory.PlayerScore > 0)
+            {
+                index = 4;
+            }
         }
-        else if (index == 5)
+        if (index == 4)
         {
-            tutorialText.text = "You're ready to play! When you're ready, click Let's Play!";
-            next.gameObject.SetActive(false);
+            tutorialText.text = "You've completed the tutorial! \n When you're ready to continue, click Let's Play!";
             play.gameObject.SetActive(true);
         }
-    }
-    public void Next()
-    {
-        index += 1;
-
-    }
-    public void Previous()
-    {
-        index -= 1;
     }
     public void Play()
     {
