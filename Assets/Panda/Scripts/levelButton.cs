1670688aea00ab6233f5312886337054f791bb63
caammyy <cammylimzy@gmail.com>
2023-03-15 06:29:37 +0800

    upd

--- Assets/Panda/Scripts/levelButton.cs
+++ Assets/Panda/Scripts/levelButton.cs
@@ -10,8 +10,6 @@ public class levelButton : MonoBehaviour
     public void OpenScene()
     {
         SoundManager.Instance.PlaySFX("Start");
-        Debug.LogWarning(PlayerPrefs.GetInt("levelsunlocked"));
-        Debug.LogWarning(PlayerPrefs.GetInt("currentLevel"));
         if (PlayerPrefs.GetInt("levelsunlocked") >= PlayerPrefs.GetInt("currentLevel"))
         {
             endTransition.SetActive(true);
